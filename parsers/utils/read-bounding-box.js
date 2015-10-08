3e958fa5d4764fcb9babce9f0892e92ec44e5be8
Emil Bay <github@tixz.dk>
2015-10-08 22:52:03 +0200

    Change caseing to conform with shp header

--- parsers/utils/read-bounding-box.js
+++ parsers/utils/read-bounding-box.js
@@ -1,9 +1,9 @@
 'use strict'
 
 module.exports = function readBoundingBox (buffer, offset) {
-  const Xmin = buffer.readDoubleLE(offset, true)
-  const Ymin = buffer.readDoubleLE(offset + 8, true)
-  const Xmax = buffer.readDoubleLE(offset + 16, true)
-  const Ymax = buffer.readDoubleLE(offset + 24, true)
-  return {Xmin, Ymin, Xmax, Ymax}
+  const xMin = buffer.readDoubleLE(offset, true)
+  const yMin = buffer.readDoubleLE(offset + 8, true)
+  const xMax = buffer.readDoubleLE(offset + 16, true)
+  const yMax = buffer.readDoubleLE(offset + 24, true)
+  return {xMin, yMin, xMax, yMax}
 }
