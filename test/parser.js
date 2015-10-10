c290a46879ee6e055823e3366bfb5c1973ddf056
Andreas Madsen <amwebdk@gmail.com>
2015-10-10 16:38:59 +0200

    fix _readBytes, the readable event does not guarantee  has been read

--- test/parser.js
+++ test/parser.js
@@ -15,7 +15,7 @@ interpreted({
   run: ['null', 'points', 'multipoints', 'empty', 'date-property',
         'boolean-property', 'mixed-properties', 'number-property',
         'string-property', 'latin1-property', 'utf8-property',
-        'ignore-properties'],
+        'ignore-properties', 'dagi_kommune'],
 
   test: function (name, callback) {
     const dirPath = path.resolve(__dirname, 'source', name)
